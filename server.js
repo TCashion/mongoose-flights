38579beee465ae8116a5ba4f73eea966148393e2
TCashion <58757351+TCashion@users.noreply.github.com>
2020-05-24 16:23:49 -0600

    install and mount method override

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const methodOverride = require('method-override');
 
 //connect to database module
 require('./config/database');
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_mehtod'));
 
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
